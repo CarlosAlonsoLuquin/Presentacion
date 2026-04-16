7d6d08ceb0bd1a58fb344125a159273d3b62b393
Yellow <davigonzalezdev@gmail.com>
2026-04-15 21:48:44 -0700

    añadir diapositiva de broma y actualizar contador

--- script.js
+++ script.js
@@ -74,5 +74,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Initialize display values
+    if(document.getElementById('total')) document.getElementById('total').innerText = totalSlides;
     updatePresentation();
 });
